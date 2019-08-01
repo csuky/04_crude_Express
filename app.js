174a19cd055709b7ed916c946998fd6979424441
kangyang <kangyangtju@qq.com>
2019-08-01 17:19:47 +0800

    添加学生信息的增、删、改功能

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*
+/**
 * app.js是入门模块，职责主要是：
 * 创建服务
 * 做一些服务相关配置（模板引擎、body-parser解析表单post请求体、提供静态资源服务）
@@ -6,14 +6,23 @@
 * 监听端口启动服务
 */
 var express = require('express');
+var router = require('./router');
+var bodyParser = require('body-parser');
+
 var app = express();
-var router = require('./router')
 
 app.use('/node_modules', express.static('./node_modules/'));
 app.use('/public', express.static('./public/'));
 
 app.engine('html', require('express-art-template'));
 
+// 配置 body-parser 中间件（插件，专门用来解析表单 POST 请求体）
+// 配置模板引擎和 body-parser 一定要在 app.use(router) 挂载路由之前
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+// 将路由器挂载到app服务中
 app.use(router);
 
 
--- router.js
+++ router.js
@@ -1,26 +1,135 @@
-/*router.js是路由模块，主要职责：
+/**router.js是路由模块，主要职责：
 * 处理路由
 * 根据不同的请求方法+请求路径设置具体的请求处理函数
 */
 var fs = require('fs');
 var express = require('express');
+
+var dbPath = './db.json';
 var router = express.Router();
 
+/**渲染学生列表页面*/
 router.get('/', function (req, res) {
-    fs.readFile('db.json', 'utf8', function (err, data) {
+    fs.readFile(dbPath, 'utf8', function (err, data) {
         if (err) {
             return res.status(500).send('Server error.');
         };
         res.render('index.html', {
+            classManager: ['班长','团支书','学习委员'],
             students: JSON.parse(data).students
         });
     });
 });
-router.get('/students', function (req, res) {
 
-});
+/**渲染添加学生页面*/
 router.get('/students/new', function (req, res) {
-
+    res.render('new.html');
+});
+/**处理添加学生页面
+* 添加学生
+* 跳转到新增学生信息页面
+* 填写学生基本信息
+* 存储到db.json文件
+* 重定向回学生信息页面*/
+router.post('/students/new', function (req, res) {
+    fs.readFile(dbPath,'utf8', function (err, data) {
+        if (err) {
+            return res.status(500).send('Server error.');
+        };
+        var student = req.body;
+        var students = JSON.parse(data).students;
+        students.length != 0 ? student.id = students[students.length - 1].id + 1 : student.id = 1;
+        students.push(student);
+        var fileData = JSON.stringify({
+            students: students
+        });
+        fs.writeFile(dbPath, fileData, function (err) {
+            if (err) {
+                return console.log(err);
+            };
+        });
+    });
+    res.redirect('/');
 });
 
+/**渲染编辑学生页面*/
+router.get('/students/edit', function (req, res) {
+    fs.readFile(dbPath, 'utf8', function (err, data) {
+        if (err) {
+            return res.status(500).send('Server error.');
+        };
+        var students = JSON.parse(data).students;
+        var newStudent = students.find(function (item) {
+            return item.id == parseInt(req.query.id);
+        });
+        res.render('edit.html', {
+            student: newStudent
+        });
+    });
+});
+
+/**
+ * 处理编辑学生
+* 获取表单数据
+* 更新db.json文件
+* 发送响应,重定向到学生列表页面*/
+router.post('/students/edit', function (req, res) {
+    fs.readFile(dbPath, 'utf8', function (err, data) {
+        if (err) {
+            return res.status(500).send('Server error.');
+        };
+
+        var student = req.body;
+        var students = JSON.parse(data).students;
+        student.id = parseInt(student.id);
+        // 要修改谁，就需要把谁找出来，采用数组的find方法
+        var editStudent = students.find(function (item) {
+            return item.id == student.id;
+        });
+        for (var key in student) {
+            editStudent[key] = student[key];
+        };
+
+        var fileData = JSON.stringify({
+            students: students
+        });
+        fs.writeFile(dbPath, fileData, function (err) {
+            if (err) {
+                return console.log(err);
+            };
+        });
+    });
+    res.redirect('/');
+});
+
+/**
+ * 处理删除学生
+ */
+router.get('/students/delete', function (req, res) {
+    fs.readFile(dbPath, 'utf8', function (err, data) {
+        if (err) {
+            return res.status(500).send('Server error.');
+        }
+        ;
+
+        var students = JSON.parse(data).students;
+        //找出要删除的元素在数组中的下标
+        var deleteStudentId = students.findIndex(function (item) {
+            return item.id === parseInt(req.query.id)
+        });
+        //根据下标删除数组中对应的学生对象
+        students.splice(deleteStudentId, 1);
+
+        var fileData = JSON.stringify({
+            students: students
+        });
+        fs.writeFile(dbPath, fileData, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            ;
+        });
+    });
+    res.redirect('/');
+});
 module.exports = router;
